5a1872494ed5d06aeac9140211dd3a95961bfed7
Jurek <git@jurek.io>
2023-01-15 16:52:18 +0100

    adding dirty CORS

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/swagger"
 
@@ -29,6 +30,16 @@ func StartServer() {
 	middleware.InitSessions()
 
 	app.Use(logger.New())
+
+	// Default config
+	app.Use(cors.New())
+
+	// Or extend your config for customization
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost, https://stalko.tk, http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	api := app.Group("/api")
 	api.Use(middleware.Session)
 	{
